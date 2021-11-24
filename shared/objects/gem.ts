81e1706ddebb3586d6f9e498b70ad8cbfc8d6076
lisajoy <lisaharrylock@gmail.com>
2021-11-24 11:19:48 -0800

    add points to gem

--- shared/objects/gem.ts
+++ shared/objects/gem.ts
@@ -1,7 +1,8 @@
-import { AsteroidDTO, GameObjectType } from "../types";
+import { AsteroidDTO, GameObjectType, GemDTO } from "../types";
 import { GameObject } from "./game-object";
 
 export class Gem extends GameObject {
+    public readonly points = 1;
     constructor(props: AsteroidDTO) {
         super({...props, type: GameObjectType.Gem});
     }
