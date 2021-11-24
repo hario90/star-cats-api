bdbce6d2201fc7000c8723bdf6deb45743382dc4
lisajoy <lisaharrylock@gmail.com>
2021-11-24 08:11:06 -0800

    move asteroid constants

--- shared/constants.ts
+++ shared/constants.ts
@@ -2,3 +2,7 @@ export const BOARD_WIDTH = 3200;
 export const BOARD_HEIGHT = 2000;
 export const halfShipWidth = 16;
 export const halfShipHeight = 15;
+
+export const MAX_ASTEROID_SPEED = 15;
+export const MAX_ASTEROID_HEIGHT = 100;
+export const MIN_ASTEROID_HEIGHT = halfShipHeight * 2;
