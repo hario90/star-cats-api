33254070db6dac95167aa42c7e54c3a844e8e194
Lisa Harrylock <lisaharrylock@gmail.com>
2026-08-23 15:50:50 -0700

    fix xml parsing

--- ui/objects/xml-drawing-utils.ts
+++ ui/objects/xml-drawing-utils.ts
@@ -3,37 +3,63 @@ import { ImageComponent } from "../component";
 import { Canvas } from "../game-engine/canvas";
 
 const parser = new DOMParser();
+
+type SubTexture = {
+  name: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+let textureMap: ReadonlyMap<string, SubTexture> | undefined = undefined;
+
+function createTextureAtlas(rawXML: string) {
+    if (textureMap) {
+        return textureMap;
+    }
+
+    const doc = parser.parseFromString(rawXML, "text/xml");
+
+    const textures = new Map<string, SubTexture>();
+
+    for (const element of doc.getElementsByTagName("SubTexture")) {
+        const name = element.getAttribute("name");
+
+        if (!name) {
+            continue;
+        }
+
+        textures.set(name, {
+            name,
+            x: Number(element.getAttribute("x")),
+            y: Number(element.getAttribute("y")),
+            width: Number(element.getAttribute("width")),
+            height: Number(element.getAttribute("height")),
+        });
+    }
+
+    return textures;
+}
+
 export const getImageComponentFromXML = (
     allAssets: any, // TODO
     rawXML: string,
     objectName: string,
     props: GameObjectDTO & { canvas: Canvas }
-) => {
-    const xmlDoc = parser.parseFromString(rawXML, "text/xml");
-    const xmlElementMatches = xmlDoc.getElementsByName(objectName);
-    if (!xmlElementMatches.length) {
+): ImageComponent | undefined => {
+    const atlas = createTextureAtlas(rawXML);
+    const subTexture = atlas.get(objectName);
+    if (!subTexture) {
         return undefined;
     }
-
-    const objectEl = xmlElementMatches[0];
-    const xStr = objectEl.getAttribute("x");
-    const yStr = objectEl.getAttribute("y");
-    const heightStr = objectEl.getAttribute("height");
-    const widthStr = objectEl.getAttribute("width");
-    if (heightStr && widthStr && xStr && yStr) {
-        const height = parseInt(heightStr, 10);
-        const width = parseInt(widthStr, 10);
-        const x = parseInt(xStr, 10);
-        const y = parseInt(yStr, 10);
-        return new ImageComponent({
-            ...props,
-            src: allAssets,
-            srcWidth: width,
-            srcHeight: height,
-            frame: 0,
-            frameLocations: [[x, y]],
-        });
-    }
-
-    return undefined;
+   
+    return new ImageComponent({
+        ...props,
+        src: allAssets,
+        srcWidth: subTexture.width,
+        srcHeight: subTexture.height,
+        frame: 0,
+        frameLocations: [[subTexture.x, subTexture.y]],
+    });
 };
